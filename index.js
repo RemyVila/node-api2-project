95dbf53ba1ffea44e5ca36685bd314ab9c4ab367
Remy Vila <josephvila123@gmail.com>
2021-03-10 01:51:26 -0500

    let's gooooo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
